4af0d4e7c8bb337e6693488890dea0cedd5a0f45
IshanChemjong <chemjongishan@gmail.com>
2022-05-14 17:21:36 +0545

    All files

---
+++ frontend/src/Components/Home/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import NavBar from "./Nav";
+
+const Home = () => {
+  return <NavBar />;
+};
+
+export default Home;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const darkTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
   },
 });
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+  <ThemeProvider theme={darkTheme}>
+    <App />
+  </ThemeProvider>
 );
